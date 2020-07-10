b35db7778a4f17d80104736a2aa39faa197f7196
Francisco Olivero <cisco.olivero@gmail.com>
2020-07-10 20:36:23 -0300

    Entrega final

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ function myFunction() {
     x.style.display = "block";
   }
 } 
+
+
+
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
